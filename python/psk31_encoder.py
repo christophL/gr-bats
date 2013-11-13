d99a10394d4381e2ac5aeac50c2aa3fd6f52488c
Bastian Bloessl <bloessl@ccs-labs.org>
2013-11-13 15:19:59 +0100

    add message input to encoder

--- python/psk31_encoder.py
+++ python/psk31_encoder.py
@@ -3,10 +3,9 @@ import numpy
 import pmt
 import sys
 import digimodes
+import struct
 
 from gnuradio import gr, blocks
-
-mystring = "sitting in my english garden waiting for the sun\n"
 
 class psk31_encoder(gr.sync_block):
     def __init__(self):
@@ -17,27 +16,45 @@ class psk31_encoder(gr.sync_block):
             out_sig = [numpy.uint8],
         )
 
-        self.tx_string = "00000000"
-        for c in mystring:
-            self.tx_string += digimodes.varicodes[c]
-            self.tx_string += "00"
-        self.tx_string += "11111111"
+        self.message_port_register_in(pmt.intern('in'))
+        self.set_msg_handler(pmt.intern('in'), self.handle_msg)
+        self.tx_string = ""
 
-        while len(self.tx_string) % 8 != 0:
-            self.tx_string += "1"
 
-        self.index = 0
+    def encode_string(self, string):
+        s = "00000000"
+        for c in string:
+            s += digimodes.varicodes[c]
+            s += "00"
+        s += "111111111111111111111111111111111111111"
+
+        while len(s) % 8 != 0:
+            s += "1"
+
+        self.tx_string += s
+
+    def handle_msg(self, msg):
+        if(pmt.is_pair(msg)):
+             blob = pmt.cdr(msg)
+             s = ""
+             for i in pmt.u8vector_elements(blob):
+                 s += chr(i)
+             print s
+             self.encode_string("\n\n\n" + s + "\n\n\n")
 
 
     def work(self, input_items, output_items):
-
-        index = self.index
+        items = 0
 
         for i in range(len(output_items[0])):
-            u8 = ~int(self.tx_string[8*index:8*(index+1)], 2) % 2 **8
-            index = (index + 1) % (len(self.tx_string) / 8)
-            output_items[0][i] = u8
+            if(len(self.tx_string) == 0):
+                break
 
-        self.index = index
-        return len(output_items[0])
+            u8 = ~int(self.tx_string[0:8], 2) % 2 **8
+            output_items[0][i] = u8
+            self.tx_string = self.tx_string[8:]
+            items += 1
+
+        print "remaining string: " + self.tx_string
+        return items
 
