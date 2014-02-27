8230b8cb4624883bfca78cb75da7901a56c10d7b
Christoph leitner <c.leitner@student.uibk.ac.at>
2014-02-27 11:31:22 +0100

    added start scripts for running the transceiver using different USRPs

---
+++ examples/1start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./bats_transceiver.py --ip='addr=192.168.10.2' --port=52001 --foff=3000
---
+++ examples/2start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./bats_transceiver.py --ip='addr=192.168.10.3' --port=52002 --foff=3000
